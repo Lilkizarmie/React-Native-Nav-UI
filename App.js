add573f16442bf00f5999846e6f6282a852c75b4
Lilkizarmie <jefferyadolor18@gmail.com>
2022-10-12 13:41:55 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
 
-export default function App() {
+import Colors from './constants/Colors';
+
+
+import AnimTab1 from './bottomTab/AnimTab1';
+import AnimTab2 from './bottomTab/AnimTab2';
+import AnimTab3 from './bottomTab/AnimTab3';
+import Home from './ButtomTab/Home';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    flex: 1,
+    backgroundColor: isDarkMode ? Colors.black : Colors.white,
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={Colors.white} />
+        <NavigationContainer>
+          <RootStack />
+        </NavigationContainer>
+      </SafeAreaView>
+    </Provider>
   );
+};
+
+const options = {
+  gestureEnabled: true,
+  gestureDirection: 'horizontal',
+  cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+  headerShown: false,
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createSharedElementStackNavigator();
+
+const RootStack = () => {
+  return (
+    <Stack.Navigator screenOptions={options}>
+      <Stack.Screen name="Home" component={Home}
+        options={{ title: 'React-Native Ui', headerShown: true }} />
+      <Stack.Screen name="Tab1" component={AnimTab1} />
+      <Stack.Screen name="Tab2" component={AnimTab2} />
+      <Stack.Screen name="Tab3" component={AnimTab3} />
+    </Stack.Navigator>
+  )
+}
+
+export default App;
---
+++ ButtomTab/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { List } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native'
+
+const Home = ({ navigation }) => {
+
+  const navigate = (route) => navigation.navigate(route)
+  return (
+    <View styles={{ flex: 1 }}>
+      <List.Accordion
+        title="Bottom Tab Navigation"
+        left={props => <List.Icon {...props} icon="folder" />}>
+        <List.Item title="Animatable Tab1" onPress={() => navigate('Tab1')} />
+        <List.Item title="Animatable Tab2" onPress={() => navigate('Tab2')} />
+        <List.Item title="Animatable Tab3" onPress={() => navigate('Tab3')} />
+        <List.Item title="Animatable Tab4" onPress={() => navigate('Tab4')} />
+        <List.Item title="Animatable Tab5" onPress={() => navigate('Tab5')} />
+      </List.Accordion>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
